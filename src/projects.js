eee66d065b517f5432001a8fde251ee6bdde3d65
owencyr <cyroc@dukes.jmu.edu>
2019-05-14 12:19:15 -0400

    migrated files from v1

---
+++ src/projects.js
@@ -0,0 +1,26 @@
+const PROJECTS = [
+  {
+    title: 'Bookmarks App',
+    desc: `In today's world, movies are released at a fever pace.  It can be hard to remember your initial reaction to the movie before the media cycle hypes the movie up to being the greatest movie america has ever seen.
+  This bookmark manager allows the user to keep a running database of movies that they have watched recently, including the description of the movie, url, and their own rating of the movie.  Maintain a database of your movie recommendations, accessible at any time on the web.
+  `,
+    tech:
+			'<i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-js-square"></i>',
+    link: 'https://thinkful-ei-armadillo.github.io/owen-robert-bookmarks',
+    repo: 'https://github.com/thinkful-ei-armadillo/owen-robert-bookmarks',
+    screenshot: 'assets/bookmarkscreen.png'
+  },
+  {
+    title: 'Bookmarks App',
+    desc: `In today's world, movies are released at a fever pace.  It can be hard to remember your initial reaction to the movie before the media cycle hypes the movie up to being the greatest movie america has ever seen.
+  This bookmark manager allows the user to keep a running database of movies that they have watched recently, including the description of the movie, url, and their own rating of the movie.  Maintain a database of your movie recommendations, accessible at any time on the web.
+  `,
+    tech:
+			'<i class="fab fa-html5"></i> <i class="fab fa-css3-alt"></i> <i class="fab fa-js-square"></i>',
+    link: 'https://thinkful-ei-armadillo.github.io/owen-robert-bookmarks',
+    repo: 'https://github.com/thinkful-ei-armadillo/owen-robert-bookmarks',
+    screenshot: 'assets/bookmarkscreen.png'
+  }
+];
+
+module.exports = PROJECTS;
